3b39a29a1c18292d2e014c7069ac6f48327db73e
manar <manar2.mh@gmail.com>
2025-04-15 14:09:43 +0100

    provision withh SSL certificate

---
+++ client inscure/build/CMakeFiles/provision_test.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/provision_test.dir/provision.grpc.pb.cc.o"
+  "CMakeFiles/provision_test.dir/provision.grpc.pb.cc.o.d"
+  "CMakeFiles/provision_test.dir/provision.pb.cc.o"
+  "CMakeFiles/provision_test.dir/provision.pb.cc.o.d"
+  "CMakeFiles/provision_test.dir/src/provision_test_insecure.cpp.o"
+  "CMakeFiles/provision_test.dir/src/provision_test_insecure.cpp.o.d"
+  "provision.grpc.pb.cc"
+  "provision.grpc.pb.h"
+  "provision.pb.cc"
+  "provision.pb.h"
+  "provision_test"
+  "provision_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/provision_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
